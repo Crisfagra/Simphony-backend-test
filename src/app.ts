5cd71999db02552cb06ebca1738619b5969818c4
Crisfagra <cfga111@gmail.com>
2024-10-21 17:03:33 -0500

    install dependencies

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Servicios',
+      version: '1.0.0',
+      description: 'Documentación de API'
+    }
+  },
+  apis: ['./src/routes/*.ts']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
