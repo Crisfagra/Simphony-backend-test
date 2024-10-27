b75592b2e2122ce133cb07b96acfc57d2937de6a
Crisfagra <cfga111@gmail.com>
2024-10-27 13:30:41 -0500

    remove unused comments

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack)
 
--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -2,7 +2,6 @@ import { plainToInstance } from 'class-transformer'
 import { validate, ValidationError } from 'class-validator'
 import { Request, Response, NextFunction } from 'express'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function validateDto(dtoClass: any) {
   return (req: Request, res: Response, next: NextFunction) => {
     const dtoInstance = plainToInstance(dtoClass, req.body)
