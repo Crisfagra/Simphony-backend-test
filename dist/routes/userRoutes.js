5fd769ba231b52cc4ee076dd2bc9cd16beac55e2
Crisfagra <cfga111@gmail.com>
2024-10-27 13:45:03 -0500

    modify git ignore

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -7,7 +7,7 @@ const validate_1 = require("../middlewares/validate");
 const userDto_1 = require("../dto/userDto");
 const userServiceDto_1 = require("../dto/userServiceDto");
 const router = (0, express_1.Router)();
-router.post('/register', (0, validate_1.validateDto)(userDto_1.CreateUserDto), userController_1.registerUser);
+router.post('/register', auth_1.authenticate, (0, validate_1.validateDto)(userDto_1.CreateUserDto), userController_1.registerUser);
 router.post('/login', userController_1.loginUser);
 router.get('/profile', auth_1.authenticate, userController_1.getUserProfile);
 router.get('/:id/services', auth_1.authenticate, userController_1.getUserServices);
